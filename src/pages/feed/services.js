5e2f235b54a6f3cc3609574314c88f69ce78cf96
Laís Ayume Lima Mune <83236574+laismune@users.noreply.github.com>
2021-08-15 12:42:55 -0300

    ajuste import errros

--- src/pages/feed/services.js
+++ src/pages/feed/services.js
@@ -1,4 +1,4 @@
-import { getError } from '../../lib/errors';
+import { getError } from '../../lib/errors.js';
 
 export const getPosts = (createAndPrintAllPosts) => {
   firebase.firestore().collection('posts').orderBy('data', 'desc').get()
