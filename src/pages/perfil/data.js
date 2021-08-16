38d19b103df0664f60b9bbeba8809a89cc3f9352
Lediane <lediane141@gmail.com>
2021-08-16 16:37:39 -0300

    atualizando conflitos

--- src/pages/perfil/data.js
+++ src/pages/perfil/data.js
@@ -40,37 +40,3 @@ export const goBackToFeed = () => {
 };
 
 
-
-/*import { getTheRoad } from "../../router.js";
-
-export const user = (nome, url) => {
-    const user = firebase.auth().currentUser;
-    user.updateProfile({
-      displayName: nome,
-      photoURL: url,
-    }).then(() => {
-      console.log('Perfil atualizado')
-      console.log(url)
-    }).catch((error) => {
-      console.log(error);
-    });
-  };
-  
-  export const perfilImage = (photo, callback) => {
-    const file = photo.files[0];
-    const storageRef = firebase.storage().ref('imagens/' + file.name);
-  
-    storageRef.put(file).then(() => {
-      storageRef.getDownloadURL().then((url) => {
-        callback(url);
-      });
-    });
-  };
-  
-  export const back = () => {
-    getTheRoad("/feed");
-  };
-  
-
-  
- */
